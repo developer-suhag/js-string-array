7c33d87943c0f1ff91d98beca37cb9f79d86ec7e
developer-suhag <developersuhag@gmail.com>
2021-08-18 23:08:04 +0600

    case convertion

---
+++ case-convert.js
@@ -0,0 +1,14 @@
+const anthem = "Amar Sonar Bangla Ami Tomay Valobashi";
+const search = 'valo';
+
+if (search.toUpperCase == anthem.toUpperCase) {
+    console.log('Ciro Dine Tomar Akas Tomar Batas');
+}
+
+const inputedUserName = 'SUhagmOktA';
+const savedUserName = 'suhagMokta';
+// console.log(inputedUserName.toLowerCase());
+// console.log(savedUserName.toUpperCase());
+if (inputedUserName.toLowerCase == savedUserName.toLowerCase) {
+    console.log('User Exists');
+}
