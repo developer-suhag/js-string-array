85b95060a8956a0f34b30cf9a25aa7797b8f5321
developer-suhag <developersuhag@gmail.com>
2021-08-19 20:13:01 +0600

    try catch

---
+++ try-catch.js
@@ -0,0 +1,18 @@
+const name = 'ami';
+// name = 'tomi'
+
+// for (let i = 0; i < array.length i++) {
+//     const element = array[i];
+
+// }
+// console.log(name());
+
+const object = undefined;
+// console.log(object.name);
+
+try {
+    name = 'tomi'
+} catch (error) {
+    console.log('Got the Error', error);
+}
+console.log(name);
